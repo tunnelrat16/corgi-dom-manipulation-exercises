ec9e486b073c0ade18149a7d47c46e309550452d
tunnelrat16 <tjandmatt@hotmail.com>
2022-03-31 00:07:04 -0700

    update 4 through 6

---
+++ exercises/4/index.js
@@ -0,0 +1,5 @@
+const liList = document.querySelectorAll("li")
+liList.forEach(li => {
+    li.classList = 'corgi'
+});
+li[0]
---
+++ exercises/5/index.js
@@ -0,0 +1,3 @@
+const ul = document.querySelector("ul")
+ul.children[0].textContent = "Cardigan";
+ul.children[1].textContent = "Pembroke"
